9202ac21479044f62cff26ca822279bf843ea169
AC <ac@marketmining.com.br>
2025-06-30 14:15:38 -0300

    feat: add example

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
 from loguru import logger
 
+from docling.document_converter import DocumentConverter
+
 
 def main():
     logger.info("running job")
+
+    converter = DocumentConverter()
+    doc = converter.convert("https://arxiv.org/pdf/2501.17887")
+    # doc.document.export_to_markdown()
+    output = doc.document.export_to_dict()
+    print(output)
 
 
 if __name__ == "__main__":
